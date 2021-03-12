3b53d4646282e20d27d46819ae21158e66bfe96e
Sandakov91 <gap-project@yandex.ru>
2021-03-12 13:39:46 +0300

    Added Unity project

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private static int platformsAmount;
+    private static Platform currentPlatform;
+
+    private int maxPlatformsAmount = 40;
+    private bool isApplicationClosed;
+
+    [SerializeField] private Transform leftSpawnPoint;
+    [SerializeField] private Transform rightSpawnPoint;
+    [SerializeField] private GameObject leftPlatform;
+    [SerializeField] private GameObject rightPlatform;
+
+    private float fallingSpeed;
+    private void OnApplicationQuit()
+    {
+        isApplicationClosed = true;
+        platformsAmount = 0;
+    }
+    private void OnDestroy()
+    {
+        if (!isApplicationClosed)
+        {
+            platformsAmount--;
+            currentPlatform.SpawnRandomPlatform();
+        }
+    }
+    void Start()
+    {
+        fallingSpeed = 0f;
+        platformsAmount++;
+        currentPlatform = this;
+        if (currentPlatform == this && platformsAmount <= maxPlatformsAmount)
+        {
+            SpawnRandomPlatform();
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.GetComponent<Player>())
+        {
+            DestroyPlatform();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.y < -10f)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector3.down * fallingSpeed * Time.deltaTime);
+    }
+
+    private void SpawnRandomPlatform()
+    {
+        if(Random.Range(0, 2) == 0)
+        {
+            GameObject newPlatform = Instantiate(leftPlatform, leftSpawnPoint.position,leftSpawnPoint.rotation);
+            newPlatform.transform.SetParent(null);
+        }
+        else
+        {
+            GameObject newPlatform = Instantiate(rightPlatform, rightSpawnPoint.position, rightSpawnPoint.rotation);
+            newPlatform.transform.SetParent(null);
+        }
+    }
+    private void DestroyPlatform()
+    {
+        fallingSpeed = 2f;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float startSpeed;
+    [SerializeField] private float speedMultiplier;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private int multiplyScore;
+
+    public int score { get; private set; }
+    private float playerSpeed;
+    private Vector3 playerDirection;
+    private Rigidbody playerRigidbody;
+
+    private float startY;
+    private float gameEndY = 0.05f;
+    void Start()
+    {
+        startY = transform.position.y;
+        playerSpeed = startSpeed;
+        playerDirection = Vector3.zero;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangePlayerDirection();
+        }
+        if(score == multiplyScore)
+        {
+            multiplyScore += multiplyScore;
+            AddSpeed();
+        }
+        if(transform.position.y < startY - gameEndY)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = playerDirection * playerSpeed;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.GetComponent<Platform>())
+        {
+            score++;
+        }
+    }
+
+    private void ChangePlayerDirection()
+    {
+        if (playerDirection == Vector3.zero || playerDirection == Vector3.forward)
+        {
+            playerDirection = Vector3.right;
+        }
+        else
+        {
+            playerDirection = Vector3.forward;
+        }
+    }
+    private void AddSpeed()
+    {
+        if (playerSpeed != maxSpeed)
+        {
+            playerSpeed *= speedMultiplier;
+        }
+        if (playerSpeed > maxSpeed)
+        {
+            playerSpeed = maxSpeed;
+        }
+    }
+}
